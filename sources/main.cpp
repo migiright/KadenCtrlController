746a92f0a74838e392bcb2037eb13b1eb42e0f5e
migiright <migigit@yahoo.co.jp>
2016-09-04 15:18:57 +0900

    シグナルハンドラの追加

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 
 #include "config.h"
 #include "socket.h"
 
 using namespace std;
 
+namespace {
+	volatile sig_atomic_t receivedSignal = 0;;
+}
+
+//シグナルハンドラ ctrl+cが押された時に呼ばれる
+void signalHandler(int signum){
+	receivedSignal = 1;
+}
+
 int main(int argc, char **argv){
+	
+	{ //シグナルハンドラの登録
+		struct sigaction sa;
+		sa.sa_handler = signalHandler;
+		sa.sa_flags = SA_RESTART;
+		sigaction(SIGINT, &sa, nullptr);
+	}
+	
 	Config config = loadConfig();
 	
 	int soc = setupClient(config.hostName, config.port);
